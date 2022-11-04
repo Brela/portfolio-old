f21492898676696bf10f4ec671dd344a41eccf9e
Breland <claybreland1@gmail.com>
2022-11-04 09:01:29 -0500

    updates

--- js/script.js
+++ js/script.js
@@ -17,4 +17,68 @@ $(document).ready(function () {
                 }
             });
     });
+
+    // scroll proposal left - right
+    const container = document.querySelector('.proposalPicsContainer')
+    const buttonRight = document.querySelector('.slide-right');
+    const buttonLeft = document.querySelector('.slide-left');
+    let width = document.querySelector('.proposalPicsContainer img').offsetWidth
+
+    buttonRight.onclick = function () {
+        container.scrollLeft += width;
+    };
+    buttonLeft.onclick = function () {
+        container.scrollLeft -= width;
+    };
+
+    /*
+        const imgs = document.querySelector('.proposalPicsContainer')
+       
+        const img = document.querySelectorAll('.imgs img')
+    
+        let idx = 0
+        let interval = setInterval(run, 2000);
+    
+        function run() {
+            idx++
+            changeImage()
+        }
+    
+        function changeImage() {
+            if (idx > img.length - 1) {
+                idx = 0
+            } else if (idx < 0) {
+                idx = img.length - 1
+            }
+    
+            imgs.style.transform = `translateX(${-idx * 500}px)`
+        }
+    
+        function resetInterval() {
+            clearInterval(interval)
+            interval = setInterval(run, 2000)
+        }
+    
+         rightBtn.addEventListener('click', () => {
+            idx++
+            changeImage()
+            resetInterval()
+        })
+    
+        leftBtn.addEventListener('click', () => {
+            idx--
+            changeImage()
+            resetInterval()
+        })
+     */
+
+
+
+
+
+
+
+
+
+
 });
